c5f390d0786ae134c4a7d9742776825de6a35361
Dipayan Saha <sahadipayan36@gmail.com>
2025-06-28 14:56:14 +0530

    Updated the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+import Home from './components/Home'
+
+function App() {
+  
+
+  return (
+    <div className='w-full min-h-screen'>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
+
