2f7e1eb220ac8200145fbee25ece207725d0bc1a
Dipayan Saha <sahadipayan36@gmail.com>
2025-07-05 13:05:28 +0530

    Updated content type

--- backend/app/schemas/tweet.py
+++ backend/app/schemas/tweet.py
@@ -1,11 +1,15 @@
 from pydantic import BaseModel
 from typing import Optional
+from sqlmodel import SQLModel, Field, Column
+from sqlalchemy.dialects.postgresql import JSON
+from typing import Dict
 
 class PromptInput(BaseModel):
     topic: str
 
 class TweetContent(BaseModel):
-    content: str
+    # content: str
+    content: Dict = Field(sa_column=Column(JSON))
 
 class TweetUpdate(BaseModel):
     topic: Optional[str] = None
